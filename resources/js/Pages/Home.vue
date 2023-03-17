4cede6b66269c6d2ab64ebbf9b3618f04a563772
George Hoffman <george.hoffman@gmail.com>
2023-03-17 13:45:13 -0700

    Episode 3 -- end

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-extrabold text-black">Hello, {{ name }}!</h1>
+    </div>
+    <p>In this series, we'll use the following frameworks:</p>
+    <ol>
+        <li v-for="fw in frameworks">{{ fw }}</li>
+    </ol>
+</template>
+
+<script setup>
+defineProps({
+    name: String,
+    frameworks: Array
+});
+</script>
